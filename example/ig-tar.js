acddadd3e574148100034900e1b80d1c801cab7f
isaacs <i@izs.me>
2012-03-26 22:13:57 -0700

    Some examples

--- example/ig-tar.js
+++ example/ig-tar.js
@@ -1,6 +1,11 @@
 // this will show what ends up in the fstream-npm package
-var P = require("../")
-P({ path: "./" })
+var P = require("fstream-ignore")
+var tar = require("tar")
+function f (entry) {
+  return entry.basename !== ".git"
+}
+
+new P({ path: "./", type: "Directory", Directory: true, filter: f })
   .on("package", function (p) {
     console.error("package", p)
   })
@@ -10,3 +15,5 @@ P({ path: "./" })
   .on("entry", function (e) {
     console.error(e.constructor.name, e.path.substr(e.root.path.length + 1))
   })
+  .pipe(tar.Pack())
+  .pipe(process.stdout)
